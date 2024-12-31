006119afd0b79bcfd59fbfa1b98be7eefd7c804c
Matthew Law <staringskyward@gmail.com>
2024-12-31 20:14:22 +0000

    Version 0.2

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,40 @@
-# build stage
-FROM rust:1.83-slim-bookworm AS builder
+# Build stage
+FROM rust:latest AS builder
+
+RUN update-ca-certificates
+
+# Create unprivileged app user
+ENV USER=kamarust
+ENV UID=10001
+
+RUN adduser \
+    --disabled-password \
+    --gecos "" \
+    --home "/nonexistent" \
+    --shell "/sbin/nologin" \
+    --no-create-home \
+    --uid "${UID}" \
+    "${USER}"
+
 WORKDIR /app
+
 COPY . .
+
 RUN cargo build --release
 
-# run stage
-FROM rust:1.83-slim-bookworm
+# Final image
+FROM debian:bookworm-slim
+
+COPY --from=builder /etc/passwd /etc/passwd
+COPY --from=builder /etc/group /etc/group
+
 WORKDIR /app
+
 COPY --from=builder /app/target/release/kamarust /app/kamarust
-EXPOSE 80
-CMD ["./kamarust"]
+
+# Use an unprivileged user.
+USER kamarust:kamarust
+
+EXPOSE 3000
+
+CMD ["/app/kamarust"]
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,35 @@
-#![deny(warnings)]
-use warp::Filter;
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::middleware::Logger;
+use env_logger::Env;
 
 static BODY: &str = r#"
 <html>
     <head>
-        <title>Rust Web App Deployed with Kamal</title>
+        <title>Example Rust Web App Deployed with Kamal</title>
     </head>
     <body>
         <h1>Hello, World!</h1>
+        <p>Version 0.2</p>
     </body>
 </html>
 "#;
 
-#[tokio::main]
-async fn main() {
-    // Match any request and return the Hello World body
-    let routes = warp::any().map(move || {
-        warp::reply::html(BODY)
-    });
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body(BODY)
+}
 
-    warp::serve(routes).run(([0, 0, 0, 0], 80)).await;
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .wrap(Logger::default())
+
+
+    })
+    .bind(("0.0.0.0", 3000))?
+    .run()
+    .await
 }
